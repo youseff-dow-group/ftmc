ec278541aa463e5652e6836b2844158c69b63146
odoo-dow-team <odoo_dow_team@hotmail.com>
2025-08-04 10:36:54 +0300

    Fix

--- custom_sale_project/models/sale_order.py
+++ custom_sale_project/models/sale_order.py
@@ -274,7 +274,7 @@ class SaleOrder(models.Model):
 
         # Write header information
         header_end_col = chr(ord('A') + total_columns - 1)
-        worksheet.merge_range(f'A1:{chr(ord('A') + (total_columns // 2))}1', f'Brand: {brand}', header_format)
+        worksheet.merge_range(f"A1:{chr(ord('A') + (total_columns // 2))}1", f"Brand: {brand}", header_format)
         worksheet.merge_range(f'{chr(ord('A') + (total_columns // 2) + 1)}1:{header_end_col}1',
                               f'COPPER: AED {copper_price:.2f} /KG', header_format)
 
